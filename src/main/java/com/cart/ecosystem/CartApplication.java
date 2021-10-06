c9e9149bd952dcfdf2d0c9f35477de63a6a364f7
greatidea1 <43351597+greatidea1@users.noreply.github.com>
2021-10-06 19:59:53 +0530

    Update CartApplication.java

--- src/main/java/com/cart/ecosystem/CartApplication.java
+++ src/main/java/com/cart/ecosystem/CartApplication.java
@@ -5,5 +5,6 @@ public class CartApplication
     public static void main(String[] s)
     {
         System.out.println("Welcome to Cart Ecosystem");
+        System.out.println("This Cart application is completed");
     }
 }
